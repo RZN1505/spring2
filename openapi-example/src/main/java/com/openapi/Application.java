b86d84bcd97ccbca53c3829b0540ec394e67d00c
RZN1505 <reznickova.zlata@yandex.ru>
2020-12-08 18:09:02 +0300

    lesson4

---
+++ openapi-example/src/main/java/com/openapi/Application.java
@@ -0,0 +1,8 @@
+package com.openapi;
+
+public class Application {
+    public static void main(String[] args) {
+
+        System.out.println(PaymentOutput);
+    }
+}
---
+++ openapi-example/target/generated-sources/openapi/club/apibank/smzagg/api/PaymentOutput.java
@@ -0,0 +1,82 @@
+package club.apibank.smzagg.api;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import java.util.UUID;
+import org.openapitools.jackson.nullable.JsonNullable;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * PaymentOutput
+ */
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2020-12-01T21:46:41.388188800+03:00[Europe/Moscow]")
+
+public class PaymentOutput   {
+  @JsonProperty("id")
+  private UUID id;
+
+  public PaymentOutput id(UUID id) {
+    this.id = id;
+    return this;
+  }
+
+  /**
+   * Get id
+   * @return id
+  */
+  @ApiModelProperty(value = "")
+
+  @Valid
+
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    PaymentOutput paymentOutput = (PaymentOutput) o;
+    return Objects.equals(this.id, paymentOutput.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class PaymentOutput {\n");
+    
+    sb.append("    id: ").append(toIndentedString(id)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
